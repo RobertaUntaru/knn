04f72dff24c1dd492c8a3210a8e8c875bbe4f74f
RobertaUntaru <56247680+RobertaUntaru@users.noreply.github.com>
2020-05-22 02:51:57 +0300

    Create Utils.h

---
+++ Utils.h
@@ -0,0 +1,14 @@
+
+#include <optional>
+#include <iostream>
+#include "Node.h"
+using namespace std;
+class Utils
+{
+	// Functie pentru calcularea factorialului unui numar
+public:
+	static double calcFactorial(double value);
+
+	
+
+};
